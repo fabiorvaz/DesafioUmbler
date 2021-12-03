20959c1947d0b93a22e528d350caa4abda344aec
Fabio Vaz <fabiorvaz@gmail.com>
2021-12-03 14:25:15 -0300

    Commit inicial

---
+++ DesafioUmbler/Server/Controllers/DomainController.cs
@@ -0,0 +1,56 @@
+﻿using DesafioUmbler.Shared;
+using DnsClient;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Whois;
+using Whois.NET;
+
+namespace DesafioUmbler.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DomainController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<Domain> GetAsync(string domainName)
+        {
+            Domain domain = new Domain();
+            var response = WhoisClient.QueryAsync(domainName);
+
+            domain.Name = domainName;
+
+            var lookup = new LookupClient();
+            try
+            {
+                var result = await lookup.QueryAsync(domainName, QueryType.ANY);
+                var record = result.Answers.ARecords().FirstOrDefault();
+
+                var address = record?.Address;
+                var ip = address?.ToString();
+
+                var hostResponse = await WhoisClient.QueryAsync(ip);
+
+                domain.Ip = ip;
+                domain.WhoIs = response.Result.Raw;
+                domain.HostedAt = hostResponse.OrganizationName;
+            }
+            catch
+            {
+                string msg = "Dominio não encontrado";
+
+                domain.Ip = msg;
+                domain.WhoIs = msg;
+                domain.HostedAt = msg;
+            }
+            return domain;
+        }
+    }
+}
---
+++ DesafioUmbler/Shared/Domain.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioUmbler.Shared
+{
+    public class Domain
+    {
+            public string Name { get; set; }
+            public string Ip { get; set; }
+            public string WhoIs { get; set; }
+            public string HostedAt { get; set; }
+    }
+
+    public class DomainValidation
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Preencha um dominio.")]
+        [RegularExpression(".*\\.com", ErrorMessage = "Preencha um dominio valido.")]
+        public string? Name { get; set; }
+    }
+}
